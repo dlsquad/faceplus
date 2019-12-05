ae11edebf9792bde7fec84caf90e4ca97a01f85d
linchanglin <493043444@qq.com>
2019-12-05 11:08:07 +0800

    update table

--- pages/pic_index/table/index.js
+++ pages/pic_index/table/index.js
@@ -2,7 +2,7 @@ const app = getApp()
 const utils = require('../../../utils/util.js')
 Page({
   data: {
-    code: '',
+    code: '1111',
     listData: []
   },
   onLoad: function (options) {
@@ -10,31 +10,27 @@ Page({
   },
 
   getPositionTable(options){
+      let that = this;
       utils.showLoading()
       utils.http({
-        url: `/code/345686`
+        url: `/table/408648`
         // type: 'POST',
         // data: {
         //   code: this.data.code
         // }
       }).then((res) => {
+        console.log('getPositionTable res', res)
         utils.hideLoading()
         if (res.data.isSuccess) {
-          this.setData({
-            listData: [
-              { "code": "01", "text": "text1", "type": "type1" },
-              { "code": "02", "text": "text2", "type": "type2" },
-              { "code": "03", "text": "text3", "type": "type3" },
-              { "code": "04", "text": "text4", "type": "type4" },
-              { "code": "05", "text": "text5", "type": "type5" },
-              { "code": "06", "text": "text6", "type": "type6" },
-              { "code": "07", "text": "text7", "type": "type7" },
-              { "code": "08", "text": "text4", "type": "type4" },
-              { "code": "09", "text": "text5", "type": "type5" },
-              { "code": "10", "text": "text6", "type": "type6" },
-              { "code": "11", "text": "text7", "type": "type7" }
-            ]
+          that.setData({
+            listData: res.data.data
+            // listData: [
+            //   ['', '', '名字1', '', '', '名字1', '', '', '名字1'],
+            //   ['/', '名字2', '/', '/', '名字2', '/', '/', '名字2', '/'],
+            //   ['名字3', '/', '/', '/', '名字2', '/', '/', '名字2', '/'],
+            // ]
           })
+          console.log('this.data.listData', this.data.listData)
         } else {
           utils.showToast(res.data.msg)
         }
